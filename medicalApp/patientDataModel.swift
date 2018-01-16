ae8df2605cc51ac1488145b36b36c003121141d9
onglo <edouardlong@icloud.com>
2018-01-16 13:08:59 +0000

    working on patient data model

---
+++ medicalApp/patientDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  patientDataModel.swift
+//  medicalApp
+//
+//  Created by Edouard Long on 16/01/2018.
+//  Copyright © 2018 Edouard Long. All rights reserved.
+//
+
+import Foundation
+
+class Patient {
+    
+    var name:String?
+    var DOB:Date?
+    var doctor:String?
+    var dateAdmitted:Date?
+    var natureOfInjury:String?
+    var injurySeriousIndex:Int?
+    
+}
